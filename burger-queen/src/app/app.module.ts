b7c8db1c2c4c19f4c71ce2f0ae72a740904a71ac
Paola Andrea Ramirez <paolandre38@gmail.com>
2023-08-12 01:16:19 -0500

    Interfaz mesero casi terminada

--- burger-queen/src/app/app.module.ts
+++ burger-queen/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { MeseroComponent } from './mesero/mesero.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
+  declarations: [AppComponent, LoginComponent, MeseroComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule], //Agrega el modulo del formulario
   providers: [],
   bootstrap: [AppComponent],
