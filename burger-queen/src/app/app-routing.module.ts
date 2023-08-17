8b7afb5d5ec17e904b8b172ece613b1ab1dba187
JaviGandarillas <129618844+JaviGandarillas@users.noreply.github.com>
2023-08-16 23:38:08 -0400

    añade modal precario

--- burger-queen/src/app/app-routing.module.ts
+++ burger-queen/src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MeseroComponent } from './mesero/mesero.component';
 import { LoginComponent } from './login/login.component';
+import { ModalComponent } from './error-modal/error-modal.component';
+
 
 const routes: Routes = [
   {path:'', component:LoginComponent},
   {path:'mesero', component:MeseroComponent},
+  {path:'modal', component:ModalComponent},
+
+  
 
 ];
 
