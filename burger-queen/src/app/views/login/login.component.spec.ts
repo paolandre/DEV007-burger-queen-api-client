2faabe34150ebaf2fb96607e754957bb05372fdf
JaviGandarillas <129618844+JaviGandarillas@users.noreply.github.com>
2023-08-23 13:57:40 -0400

    test de historia de usuario 1

--- burger-queen/src/app/views/login/login.component.spec.ts
+++ burger-queen/src/app/views/login/login.component.spec.ts
@@ -1,15 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LoginComponent } from './login.component';
+import { ConsumirApiService } from '../../service/consumir-api.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing'
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [LoginComponent]
-    });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        ReactiveFormsModule,
+        HttpClientTestingModule
+      ],
+      declarations: [LoginComponent],
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA
+      ],
+    })
+    .compileComponents();
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,3 +31,4 @@ describe('LoginComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
