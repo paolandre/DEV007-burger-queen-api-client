6614e8aa97e600080e23e1255a98c8ce583348ca
Andrea Ramirez <paolandre08@gmail.com>
2023-09-23 02:38:36 -0500

    Cambios de CSS: Lista desplegable de las modales, margenes de la vista de admin, formato de la hora

--- burger-queen/src/app/views/chef/chef.component.ts
+++ burger-queen/src/app/views/chef/chef.component.ts
@@ -73,4 +73,12 @@ export class ChefComponent {
     }, 1000);
   }
   finishOrder() {}
+
+  formatTime(timeString: string): string {
+    const date = new Date(timeString);
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    return `${hours}:${minutes}`;
+  }
 }
+
