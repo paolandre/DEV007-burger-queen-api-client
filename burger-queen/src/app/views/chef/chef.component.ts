87aa7b4b43aba0dc4efe9c94fcc8d2d213bc4fc4
Your Name <you@example.com>
2023-10-03 16:36:21 -0300

    cronometro

--- burger-queen/src/app/views/chef/chef.component.ts
+++ burger-queen/src/app/views/chef/chef.component.ts
@@ -16,14 +16,12 @@ export class ChefComponent {
   orders: Array<any> = [];
   products: Array<any> = [];
   id: number = 0;
-  hora: number = 0;
-  minuto: number = 0;
-  segundo: number = 0;
-  timer: string = `${this.hora}:${this.minuto}:${this.segundo}`;
+  date: any;
 
   constructor(private consumirApi: ConsumirApiService) {}
   ngOnInit(): void {
     this.getAllOrders();
+    this.resetCronometer(this.date);
   }
 
   getAllOrders() {
@@ -56,22 +54,18 @@ export class ChefComponent {
       )}`;
 
       const productNames = data.products.map((product: any) => product.name);
-      const productQuantity = data.products.map((product: any) => product.quantity);
-
+      const productQuantity = data.products.map(
+        (product: any) => product.quantity
+      );
 
       this.detailsOrderMessage2 = `Mesa: ${data.table}`;
-      
-      this.detailsOrderMessage3 = `Pedido: ${this.products.join(' - ')}`;    
+
+      this.detailsOrderMessage3 = `Pedido: ${this.products.join(' - ')}`;
     });
 
     this.modal.openModal();
   }
 
-  timerFunction() {
-    setInterval(() => {
-      this.segundo += 1;
-    }, 1000);
-  }
   finishOrder() {}
 
   formatTime(timeString: string): string {
@@ -80,5 +74,27 @@ export class ChefComponent {
     const minutes = date.getMinutes().toString().padStart(2, '0');
     return `${hours}:${minutes}`;
   }
-}
+  // funcion para calcular tiempo de preparacion desde orden generada
+  cronometer(dataEntry: any) {
+    this.date = dataEntry;
+    const currentTime = new Date().getTime();
+    const initOrderTime = new Date(dataEntry).getTime();
+    const elapsedTime = currentTime - initOrderTime;
+    const hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    const minutes = Math.floor((elapsedTime % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((elapsedTime % (1000 * 60)) / 1000);
+    return `${this.formatoDosDigitos(hours)}:${this.formatoDosDigitos(
+      minutes
+    )}:${this.formatoDosDigitos(seconds)}`;
+  }
 
+  formatoDosDigitos(valor: number): string {
+    return valor < 10 ? `0${valor}` : valor.toString();
+  }
+
+  resetCronometer(dataEntry: any) {
+    setInterval(() => {
+      this.cronometer(dataEntry);
+    }, 1000);
+  }
+}
